4fcb9aec4b98b7eb9de8a12fe48da599929eeff0
github username <github email address>
2019-05-02 20:45:08 -0400

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,13 +5,13 @@ const NavBar = () => {
   return (
     <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
       <NavLink 
-        style={{ marginRight: '10px' }} 
+        style={{ marginRight: '10px' }}
         to="/"
       >
         Home
       </NavLink>
-      <NavLink 
-        style={{ marginRight: '10px' }} 
+      <NavLink
+        style={{ marginRight: '10px' }}
         to="/movies"
       >
         Movies
